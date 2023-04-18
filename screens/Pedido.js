f33ca279843f7231cc140da7dd242f1f49fe4ceb
larissabileski <laricristinabileski@gmail.com>
2023-04-18 16:53:10 -0300

    Inicio do Forms para Cadastro de Pedidos

--- screens/Pedido.js
+++ screens/Pedido.js
@@ -1,18 +1,36 @@
-import React from 'react';
-import { StyleSheet, View, Text} from 'react-native';
-import { Card } from 'react-native-paper';
-import { DefaultTheme } from 'react-native-paper';
+import React, {useState} from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Card, DefaultTheme, TextInput } from 'react-native-paper';
+import DropDown from "react-native-paper-dropdown";
 
-export default function Pedido () {
+const Pedido = () => {
+  const [showDropDown, setShowDropDown] = useState(false);
+
+  const [text, setCliente] = React.useState("");
+  const [open, setOpen] = useState(false);
+  const [value, setValue] = useState(null);
+  const [items, setItems] = useState([
+    {label: 'Boleto', value: 'boleto'},
+    {label: 'Cheque', value: 'cheque'}
+  ]);
   return (
     <View style={styles.container}>
       <Card style={styles.card}>
-        {/* <form>
-        <label><Text>Nome:</Text>
-          <input type="text" name="name" />
-        </label>
-        <input type="submit" value="Submit" />
-        </form> */}
+      <TextInput
+        label="Cliente"
+        value={text}
+        onChangeText={text => setCliente(text)}
+      />
+      <DropDown
+              label={"Pagamento"}
+              mode={"outlined"}
+              visible={showDropDown}
+              showDropDown={() => setShowDropDown(true)}
+              onDismiss={() => setShowDropDown(false)}
+              value={value}
+              setValue={setValue}
+              list={items}
+            />
       </Card>
 
     </View>
@@ -31,3 +49,5 @@ const styles = StyleSheet.create({
     width: '90%'
   }
 });
+
+export default Pedido;
