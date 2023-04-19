45c2f7e1bdcd6365b5fc9708fae07bd7b2e7c215
larissabileski <laricristinabileski@gmail.com>
2023-04-19 11:47:26 -0300

    Erro no DropDown

--- screens/Pedido.js
+++ screens/Pedido.js
@@ -1,53 +1,77 @@
-import React, {useState} from 'react';
-import { StyleSheet, View } from 'react-native';
-import { Card, DefaultTheme, TextInput } from 'react-native-paper';
-import DropDown from "react-native-paper-dropdown";
+import React, { FC, useState } from "react";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { Card, DefaultTheme } from "react-native-paper";
+import { Icon } from "react-native-elements";
 
-const Pedido = () => {
-  const [showDropDown, setShowDropDown] = useState(false);
+interface Props {
+  label: string;
+}
 
-  const [text, setCliente] = React.useState("");
-  const [open, setOpen] = useState(false);
-  const [value, setValue] = useState(null);
-  const [items, setItems] = useState([
-    {label: 'Boleto', value: 'boleto'},
-    {label: 'Cheque', value: 'cheque'}
-  ]);
+const Pedido: FC <Props> = ({ label }) => {
+  const [visible, setVisible] = useState(false);
+
+  const toggleDropdown = () => {
+    setVisible(!visible);
+  };
+
+  const renderDropdown = () => {
+    if (visible) {
+      return (
+        <Text style={styles.dropdown}>
+          This is where the dropdown will be rendered.
+        </Text>
+      );
+    }
+  };
+
   return (
     <View style={styles.container}>
       <Card style={styles.card}>
-      <TextInput
-        label="Cliente"
-        value={text}
-        onChangeText={text => setCliente(text)}
-      />
-      <DropDown
-              label={"Pagamento"}
-              mode={"outlined"}
-              visible={showDropDown}
-              showDropDown={() => setShowDropDown(true)}
-              onDismiss={() => setShowDropDown(false)}
-              value={value}
-              setValue={setValue}
-              list={items}
-            />
+        {/* <TextInput
+          label="Cliente"
+          value={text}
+          onChangeText={(text) => setCliente(text)}
+        /> */}
+        <TouchableOpacity style={styles.button} onPress={toggleDropdown}>
+          {renderDropdown()}
+          <Text style={styles.buttonText}>{label}</Text>
+          <Icon type="font-awesome" name="chevron-down" />
+        </TouchableOpacity>
       </Card>
-
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
+    flexDirection: "column",
     backgroundColor: DefaultTheme.colors.background,
-    alignItems: 'center',
-    paddingTop: 10
+    alignItems: "center",
+    paddingTop: 10,
   },
   card: {
-    width: '90%'
-  }
+    width: "90%",
+  },
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#efefef',
+    height: 50,
+    width: '90%',
+    paddingHorizontal: 10,
+    zIndex: 1,
+  },
+  buttonText: {
+    flex: 1,
+    textAlign: 'center',
+  },
+  dropdown: {
+    position: 'absolute',
+    backgroundColor: '#fff',
+    top: 50,
+  },
 });
 
-export default Pedido;
+export default Dropdown;
+
