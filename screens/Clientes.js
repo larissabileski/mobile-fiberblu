62b9c75e189040e9e43aa4245db095ce009fa4e5
viniihds <viniciushsilva2210@gmail.com>
2023-09-26 16:23:27 -0300

    Tela de Empresas puxando do backend

--- screens/Clientes.js
+++ screens/Clientes.js
@@ -4,34 +4,30 @@ import { Card } from "react-native-paper";
 import { DefaultTheme } from "react-native-paper";
 import empresaService from '../src/services/empresa';
 
-const [empresas, setEmpresas] = useState([])
+const Empresa = () => {
+  const [empresas, setEmpresas] = useState([]);
 
-async function fetchEmpresas(){
-  const data = await empresaService.getAllEmpresa();
-  setEmpresas(data)
+  async function fetchEmpresas(){
+    const data = await empresaService.getAllEmpresa();
+    setEmpresas(data)
 }
 
-useEffect(() => {
-  fetchEmpresas();
+  useEffect(() => { 
+    fetchEmpresas();
 })
 
-
-export default function Clientes() {
   return (
     <ScrollView style={styles.scrollView}>
       {empresas.map((empresa) => (
         <View style={styles.container}>
-        <Card style={styles.cardtop}>
-          <Card.Title title="Empresas Cadastrados" />
-        </Card>
         <Card style={styles.card}>
-          <Card.Title title="Empresa Cliente" />
+          <Card.Title title={empresa.nome} />
           <View style={styles.informacao}>
             <Text style={styles.title}>CNPJ:{empresa.cnpj}</Text>
             <Text style={styles.title}>Endereço:{empresa.endereco}</Text>
             <Text style={styles.title}>Telefone:{empresa.telefone}</Text>
             <Text style={styles.title}>Email:{empresa.email}</Text>
-            <Text style={styles.title}>Categoria Empresa:{}</Text>
+            <Text style={styles.title}>Categoria Empresa:{empresa.categoria}</Text>
           </View>
         </Card>
       </View>
@@ -59,9 +55,13 @@ const styles = StyleSheet.create({
   },
   cardtop: {
     width: "90%",
+    
   },
   title: {
     marginHorizontal: 15,
     marginBottom: 10,
   },
 });
+
+
+export default Empresa;
